24955b064bf66ebef829f590130806455c54d0f1
ShakthiSundar-K <shakthisundar01@gmail.com>
2025-05-17 18:27:54 +0530

    add blog model with schema definition for MongoDB

---
+++ backend/models/Blog.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema); // Create a model named "Blog" using the blogSchema.
+// This model will be used to interact with the "blogs" collection in the MongoDB database.
+
+export default Blog;
