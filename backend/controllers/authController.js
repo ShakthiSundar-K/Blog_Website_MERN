67002d706198d527f612e2e9a93dbc103f250a39
ShakthiSundar-K <shakthisundar01@gmail.com>
2025-05-17 19:28:59 +0530

    add signin functionality to authController for user login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,8 +15,32 @@ const signup = async (req, res) => {
 
     return res.status(201).json({ message: "User created successfully" }); // Send a 201 response indicating that the user was created successfully
   } catch (err) {
-    res.status(500).json({ message: "Error creating user" }); // If an error occurs, send a 500 response with an error message
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: err.message }); // If an error occurs, send a 500 response with an error message
   }
 };
 
-export default { signup };
+const signin = async (req, res) => {
+  const { email, password } = req.body; // Destructure the request body to get email and password
+  try {
+    const user = await User.findOne({ email }); // Find the user by email
+    if (!user) {
+      return res.status(404).json({ message: "User not found" }); // If the user is not found, send a 404 response with an error message
+    }
+    const match = await bcrypt.compare(password, user.password); // Compare the provided password with the hashed password in the database
+    if (!match) {
+      return res.status(400).json({ message: "Invalid password" }); // If the passwords do not match, send a 400 response with an error message
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    }); // Generate a JWT token with the user's ID and a secret key, set to expire in 2 hour
+    res
+      .status(200)
+      .json({ token, user: { name: user.name, email: user.email } }); // Send a 200 response with the token and user ID
+  } catch (err) {
+    res.status(500).json({ message: "Error signing in", error: err.message }); // If an error occurs, send a 500 response with an error message
+  }
+};
+
+export default { signup, signin };
