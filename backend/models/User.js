6b0b1bfd1062cc985302fec4e3d024d6d0efe5cb
ShakthiSundar-K <shakthisundar01@gmail.com>
2025-05-17 18:23:53 +0530

    add user model with schema definition for MongoDB

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensures that each email is unique in the database
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema); // Create a model named "User" using the userSchema.
+// This model will be used to interact with the "users" collection in the MongoDB database.
+
+export default User;
